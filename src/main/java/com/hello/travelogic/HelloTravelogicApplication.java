ca56f6f84ea114937d64abbfea187d668c346a95
1234Juser <1211shjung@gmail.com>
2025-04-14 18:24:52 +0900

    [docs] 스프링부트 기능 구현

---
+++ src/main/java/com/hello/travelogic/HelloTravelogicApplication.java
@@ -0,0 +1,13 @@
+package com.hello.travelogic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloTravelogicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloTravelogicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hello/travelogic/HelloTravelogicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hello.travelogic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloTravelogicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
