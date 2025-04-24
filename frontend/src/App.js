a56a0920ea0998b0a0285c37899a6da46be2f86b
1234Juser <1211shjung@gmail.com>
2025-04-24 13:01:17 +0900

    [docs] Header경로 수정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
-import MyReviewPage from "./pages/mypages/MyReviewPage";
 import {Route, Routes} from "react-router-dom";
 import HeaderCom from "./components/common/HeaderCom";
-import IndexPage from "./pages/IndexPage";
-import LikePage from "./pages/LikePage";
-import MyPage from "./pages/mypages/MyPage";
-import MyReservationPage from "./pages/mypages/MyReservationPage";
 
 function App() {
   return (
       <div className="App">
         <Routes>
-          <Route element={<HeaderCom />} />
+           <Route path="/" element={<HeaderCom />} />
           {/*<Route path="/" element={<IndexPage />} />*/}
           {/*<Route path="/like" element={<LikePage />} />*/}
           {/*<Route path="/my" element={<MyPage />} />*/}
