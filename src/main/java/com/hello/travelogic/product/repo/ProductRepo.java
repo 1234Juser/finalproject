1154f0e67908932684622c3b83762d3b11b7b0f3
YYSSJJIINN <h20171058@daum.net>
2025-04-28 17:13:42 +0900

    [feat] 찜기능 추가

--- src/main/java/com/hello/travelogic/product/repo/ProductRepo.java
+++ src/main/java/com/hello/travelogic/product/repo/ProductRepo.java
@@ -3,7 +3,10 @@ package com.hello.travelogic.product.repo;
 import com.hello.travelogic.product.domain.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProductRepo extends JpaRepository<ProductEntity, Long> {
 
-
+    // 찜 등록/취소에 사용
+    Optional<ProductEntity> findByProductCode(long productCode);
 }
