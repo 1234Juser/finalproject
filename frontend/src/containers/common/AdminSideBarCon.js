61fcd74cc8396608bcfd9d2b024de44b464cf215
1234Juser <1211shjung@gmail.com>
2025-04-28 10:21:52 +0900

    [feat] 어드민마이페이지 기능 구현

--- frontend/src/containers/common/AdminSideBarCon.js
+++ frontend/src/containers/common/AdminSideBarCon.js
@@ -5,8 +5,18 @@ const adminMenuList = [
     { label: "개인정보조회", path: "/adminmypage" },
     { label: "회원정보조회", path: "/admin/reservations" },
     { label: "상품관리", path: "/admin/contents" },
-    { label: "예약관리", path: "/admin/qna" },
-    { label: "리뷰관리", path: "/admin/event" },
+    { label: "예약관리", path: "/admin/reservations",
+    subMenu:[
+        {label: "전체조회", path: "/admin/reservations/list"},
+        {label: "상품별조회", path: "/admin/reservations/li"}
+    ]
+    },
+    { label: "리뷰관리", path: "/admin/event",
+    subMenu:[
+            {label: "전체조회", path: "/admin/review/list"},
+            {label: "상품별조회", path: "/admin/review/li"}
+    ]
+    },
     { label: "이벤트관리", path: "/admin/event" },
     { label: "FAQ관리", path: "/admin/event" },
     { label: "1:1문의관리(채팅)", path: "/admin/event" }
