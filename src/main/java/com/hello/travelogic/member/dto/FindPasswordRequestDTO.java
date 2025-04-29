6d5dac08174a3792ec0dda978c8c7dd0435b2587
1234Juser <1211shjung@gmail.com>
2025-04-29 16:28:20 +0900

    [feat] 비밀번호 일반정보로 찾기 기능 구현

---
+++ src/main/java/com/hello/travelogic/member/dto/FindPasswordRequestDTO.java
@@ -0,0 +1,18 @@
+package com.hello.travelogic.member.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class FindPasswordRequestDTO {
+    private String memberName;
+    private String memberId;
+    private String memberEmail;
+
+
+}
