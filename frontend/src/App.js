8c42ccf40be24ffff987438e607788ccfc94fafd
1234Juser <1211shjung@gmail.com>
2025-05-02 12:00:16 +0900

    [feat] 소셜로그인 구글 기능 구현

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,6 +16,8 @@ import RegisterMethodSelectPage from "./pages/member/RegisterMethodSelectPage";
 import KakaoCallbackPage from "./pages/member/KakaoCallbackPage";
 // import ProductRegPage from './pages/product/ProductRegPage';
 import WishGroupPage from "./pages/wish/WishGroupPage";
+import GoogleCallbackPage from "./pages/member/GoogleCallbackPage";
+import WishListPage from "./pages/wish/WishListPage";
 // import WishListPage from "./pages/wish/WishListPage";
 
 function App() {
@@ -25,20 +27,35 @@ function App() {
 
           <Routes>
               <Route path="/" element={<MainPage />} />
+              {/*회원가입/로그인*/}
               <Route path="/registerselect" element={<RegisterMethodSelectPage />} />
               <Route path="/register" element={<RegisterPage />} />
               <Route path="/login" element={<LoginPage />} />
               <Route path="/oauth/kakao/callback" element={<KakaoCallbackPage />} />
-          <Route path="/wish/groups" element={<WishGroupPage />} />
-          <Route path="/wish/groups/:groupCode/items" element={<WishListPage />} />
+              <Route path="/oauth/google/callback" element={<GoogleCallbackPage />} />
+
+
+              {/*<Route path="/like" element={<LikePage />} />*/}
+
+              {/*<Route path="/myreservation" element={<MyReservationPage />} />*/}
+          {/*<Route path="/myreview" element={<MyReviewPage />} />*/}
+          <Route path="/wish/groups/:memberCode" element={<WishGroupPage />} />
+          {/*<Route path="/wish/groups/:groupCode/items" element={<WishListPage />} />*/}
+
+
+              {/*  회원마이페이지*/}
               <Route path="/mypage">
                 <Route path="" element={<MyPagePage />} />
                 <Route path="withdrawl" element={<WithdrawlPage />} />
+                <Route path="/wish/groups" element={<WishGroupPage />} />
+                <Route path="/wish/groups/:groupCode/items" element={<WishListPage />} />
               </Route>
+              {/*관리자마이페이지*/}
               <Route path="/adminmypage">
                 <Route path="" element={<AdminMyPagePage />} />
                 {/*<Route path="product" element={<ProductRegPage/>} />*/}
               </Route>
+
             <Route path="/domestic" element={<DomesticPage/>}/>
               <Route path="/international" element={<InternationalPage />} />
               <Route path="/products">
