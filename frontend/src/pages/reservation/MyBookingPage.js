ba2ed52f53009a7d3b555b5a559ffbab297824f7
YYSSJJIINN <h20171058@daum.net>
2025-05-09 18:07:26 +0900

    [fix] 예약조회기능 accessToken으로 변경

--- frontend/src/pages/reservation/MyBookingPage.js
+++ frontend/src/pages/reservation/MyBookingPage.js
@@ -1,10 +1,11 @@
 import MyBookingCon from "../../containers/booking/MyBookingCon";
 
 function MyBookingPage() {
+    const accessToken = localStorage.getItem("accessToken");
 
     return(
     <>
-        <MyBookingCon />
+        <MyBookingCon accessToken={accessToken} />
     </>)
 }
 export default MyBookingPage
--- src/main/java/com/hello/travelogic/order/service/OrderService.java
+++ src/main/java/com/hello/travelogic/order/service/OrderService.java
@@ -34,19 +34,20 @@ import java.util.stream.Collectors;
 @Slf4j
 @RequiredArgsConstructor
 public class OrderService {
-    
+
     private final OrderRepo orderRepo;
     private final OptionRepo optionRepo;
     private final ReviewRepo reviewRepo;
     private final ProductRepo productRepo;
     private final MemberRepository memberRepo;
-    
+
     // 관리자의 주문조회
+    // 관리자 상품별 추가하면서 수정됐음
     @Transactional
     public Map<String, Object> getAllMemberBookingList(int start) {
-        
+
         start = start > 0? start -1 : start;
-        
+
         int size = 10;
         Pageable pageable = PageRequest.of(start, 10, Sort.by("orderDate").descending());
         Page<OrderEntity> page = orderRepo.findAllWithJoins(pageable);
@@ -69,43 +70,72 @@ public class OrderService {
         map.put("reservations", reservationList);
         map.put("totalPages", page.getTotalPages());
         map.put("currentPage", page.getNumber() + 1);
-        
+
         return map;
     }
-    
+
+    // 관리자의 주문조회
+    // 구버전인데 안지우고..
+//    @Transactional
+//    public Map<String, Object> getAllMemberBookingList(int start) {
+//
+//        start = start > 0? start -1 : start;
+//
+//        int size = 10;
+//        Pageable pageable = PageRequest.of(start, size, Sort.by(Sort.Order.desc("orderDate")));
+//        Page<OrderEntity> page = orderRepo.findAll(pageable);
+//        List<OrderEntity> listE = page.getContent();
+//
+//        LocalDate today = LocalDate.now();
+//        for (OrderEntity order : page.getContent()) {
+//            LocalDate resDate = order.getOption().getReservationDate();
+//            if (order.getOrderStatus() == OrderStatus.SCHEDULED && resDate != null && resDate.isBefore(today)) {
+//                order.setOrderStatus(OrderStatus.COMPLETED);
+//                orderRepo.save(order); // 자동 갱신
+//            }
+//        }
+//
+//        Map<String, Object> map = new HashMap<>();
+//        map.put("list", listE.stream().map(entity -> new OrderDTO(entity)).toList());
+//        map.put("totalPages", page.getTotalPages());
+//        map.put("currentPage", page.getNumber() + 1);
+//
+//        return map;
+//    }
+
     // 로그인 된 회원의 주문내역 조회
     @Transactional(readOnly = true)
     public List<OrderDTO> getRecentOrders(long memberCode) {
         MemberEntity member = memberRepo.findById(memberCode).orElseThrow();
         LocalDate cutoff = LocalDate.now().minusMonths(6);
         return orderRepo.findRecentOrders(member, cutoff).stream()
-               .map(OrderDTO::new)
-               .collect(Collectors.toList());
+                .map(OrderDTO::new)
+                .collect(Collectors.toList());
     }
-    
+
     @Transactional(readOnly = true)
     public List<OrderDTO> getOldOrders(long memberCode) {
         MemberEntity member = memberRepo.findById(memberCode).orElseThrow();
         LocalDate cutoff = LocalDate.now().minusMonths(6);
         return orderRepo.findOldOrders(member, cutoff).stream()
-               .map(OrderDTO::new)
-               .collect(Collectors.toList());
+                .map(OrderDTO::new)
+                .collect(Collectors.toList());
     }
-    
+
     @Transactional
     public Long createOrder(OrderDTO dto) {
         ProductEntity product = productRepo.findById(dto.getProductCode())
-                                .orElseThrow(() -> new IllegalArgumentException("상품이 존재하지 않습니다."));
+                .orElseThrow(() -> new IllegalArgumentException("상품이 존재하지 않습니다."));
         OptionEntity option = optionRepo.findById(dto.getOptionCode())
-                              .orElseThrow(() -> new IllegalArgumentException("옵션이 존재하지 않습니다."));
+                .orElseThrow(() -> new IllegalArgumentException("옵션이 존재하지 않습니다."));
         MemberEntity member = memberRepo.findById(dto.getMemberCode())
-                              .orElseThrow(() -> new IllegalArgumentException("회원이 존재하지 않습니다."));
-        
+                .orElseThrow(() -> new IllegalArgumentException("회원이 존재하지 않습니다."));
+
         OrderEntity order = new OrderEntity(dto, product, option, member);
         order = orderRepo.save(order);
         return order.getOrderCode();
     }
-    
+
     @Transactional(readOnly = true)
     public OrderDTO getOrder(Long orderCode) {
         OrderEntity order = orderRepo.findById(orderCode)
@@ -141,40 +171,40 @@ public class OrderService {
 //                .map(OrderDTO::new)
 //                .collect(Collectors.toList());
 //    }
-    
+
     @Transactional
     public int updateOrderStatusIfCompleted(Long orderCode) {
-        
+
         OrderEntity order = orderRepo.findById(orderCode)
-                            .orElseThrow(() -> new IllegalArgumentException("주문이 존재하지 않습니다."));
-        
+                .orElseThrow(() -> new IllegalArgumentException("주문이 존재하지 않습니다."));
+
         LocalDate resDate = order.getOption().getReservationDate();
         if (resDate != null && resDate.isBefore(LocalDate.now())) {
             order.setOrderStatus(OrderStatus.COMPLETED);
             orderRepo.save(order);
             return 1; // 상태가 바뀐 경우
         }
-        
+
         return 0;
     }
-    
+
     @Transactional
     public void cancelOrdersByAdmin(List<Long> orderCodeList) {
-        
+
         for (Long orderCode : orderCodeList) {
             OrderEntity order = orderRepo.findById(orderCode)
-                                .orElseThrow(() -> new IllegalArgumentException("해당 주문이 없습니다: " + orderCode));
-            
+                    .orElseThrow(() -> new IllegalArgumentException("해당 주문이 없습니다: " + orderCode));
+
             order.setOrderStatus(OrderStatus.CANCELED);
             orderRepo.save(order);
         }
     }
-    
+
     @Transactional
     public void cancelOrderByMember(Long orderCode, Long memberCode) {
-        
+
         OrderEntity order = orderRepo.findById(orderCode)
-                            .orElseThrow(() -> new IllegalArgumentException("주문이 존재하지 않습니다."));
+                .orElseThrow(() -> new IllegalArgumentException("주문이 존재하지 않습니다."));
         if (!order.getMember().getMemberCode().equals(memberCode)) {
             throw new SecurityException("본인의 예약만 취소할 수 있습니다.");
         }
