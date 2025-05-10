551a19c0eb54c131651ead3f046a9842a54ed24d
seulgiLee113 <cktncth55@gmail.com>
2025-05-10 16:20:39 +0900

    [feat] 채팅 기능 구현중

---
+++ frontend/src/components/community/chat/ChatMainCom.js
@@ -0,0 +1,49 @@
+import {
+    ChatBox, ChatButton, ChatForm, ChatInput,
+    ConnectionStatus,
+    Container,
+    JoinMsg,
+    LeaveMsg,
+    MessageRow
+} from '../../../style/community/chat/StyledChatMain'
+
+function ChatMainCom({isConnected, username, messages, sendMessage, newMessage, setNewMessage }) {
+
+    return (
+        <Container>
+            <h2>
+                실시간 채팅 (
+                <ConnectionStatus connected={isConnected}>
+                    {isConnected ? "연결됨" : "연결 끊김"}
+                </ConnectionStatus>
+                )
+            </h2>
+            <p>사용자: {username}</p>
+            <ChatBox>
+                {messages.map((msg, index) => (
+                    <MessageRow key={index}>
+                        <strong>{msg.sender}</strong> 
+                        ({new Date(msg.sentAt || Date.now()).toISOString()}): {msg.message}
+                        {msg.type === "JOIN" && <JoinMsg>님이 입장했습니다.</JoinMsg>}
+                        {msg.type === "LEAVE" && <LeaveMsg>님이 퇴장했습니다.</LeaveMsg>}
+                    </MessageRow>
+                ))}
+            </ChatBox>
+            <ChatForm onSubmit={sendMessage}>
+                <ChatInput
+                    type="text"
+                    value={newMessage}
+                    onChange={(e) => setNewMessage(e.target.value)}
+                    placeholder="메시지를 입력하세요..."
+                    disabled={!isConnected}
+                />
+                <ChatButton type="submit" disabled={!isConnected}>
+                    보내기
+                </ChatButton>
+            </ChatForm>
+        </Container>
+
+    )
+}
+
+export default ChatMainCom;
---
+++ frontend/src/pages/community/chat/ChatMainPage.js
@@ -0,0 +1,12 @@
+import ChatMainCon from "../../../containers/community/chat/ChatMainCon";
+
+function ChatMainPage() {
+
+    return (
+        <>
+            <ChatMainCon/>
+        </>
+    )
+}
+
+export default ChatMainPage;
