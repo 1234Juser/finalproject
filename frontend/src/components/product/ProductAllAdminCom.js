91ccca977528f016f6c0f7be3192f18b1392ac11
seulgiLee113 <cktncth55@gmail.com>
2025-05-12 11:05:44 +0900

    [fix] 상품 등록 컴포넌트 수정

--- frontend/src/components/product/ProductAllAdminCom.js
+++ frontend/src/components/product/ProductAllAdminCom.js
@@ -1,9 +1,9 @@
 import {containerStyle, mainStyle, sidebarStyle} from "../../style/member/MyPageStyle";
 import AdminSideBarPage from "../../pages/common/AdminSideBarPage";
-import {StyledTable, TableContainer} from "../../style/product/StyledProductAllAdmin";
+import {StyledTable, TableContainer, PaginationContainer, NavButton} from "../../style/product/StyledProductAllAdmin";
 import {Link} from "react-router-dom";
 
-function ProductAllAdminCom({products, currentPage, totalPages, totalItems, onPageChange, onDelete}) {
+function ProductAllAdminCom({products, handlePrev, handleNext, renderPageButtons, pageRange, totalPages, onDelete}) {
 
     return (
         <div style={containerStyle}>
@@ -13,110 +13,81 @@ function ProductAllAdminCom({products, currentPage, totalPages, totalItems, onPa
             <main style={mainStyle}>
                 <h1>전체 상품 목록</h1>
                 <hr />
-                    <div style={{ marginTop: "20px", textAlign: "left" }}>
-                        <button
-                            onClick={() => onPageChange(1)}
-                            disabled={currentPage === 1}
-                            style={{
-                                margin: "0 5px",
-                                padding: "5px 10px",
-                                backgroundColor: currentPage === 1 ? "lightgray" : "white",
-                                border: "1px solid black",
-                                cursor: currentPage === 1 ? "not-allowed" : "pointer",
-                            }}
-                        >
-                            {"<<"}
-                        </button>
-                        {Array.from({ length: totalPages }, (_, index) => (
-                            <button
-                                key={index + 1}
-                                onClick={() => onPageChange(index + 1)}
-                                style={{
-                                    margin: "0 5px",
-                                    padding: "5px 10px",
-                                    backgroundColor: currentPage === index + 1 ? "lightgray" : "white",
-                                    border: "1px solid black",
-                                    cursor: currentPage === index + 1 ? "not-allowed" : "pointer",
-                                }}
-                            >
-                                {index + 1}
-                            </button>
-                        ))}
-                        <button
-                            onClick={() => onPageChange(totalPages)}
-                            disabled={currentPage === totalPages}
-                            style={{
-                                margin: "0 5px",
-                                padding: "5px 10px",
-                                backgroundColor: currentPage === totalPages ? "lightgray" : "white",
-                                border: "1px solid black",
-                                cursor: currentPage === totalPages ? "not-allowed" : "pointer",
-                            }}
-                        >
-                            {">>"}
-                        </button>
-                    </div>
+                <PaginationContainer>
+                    <NavButton onClick={handlePrev} disabled={pageRange.start === 1}>
+                        &lt;&lt;
+                    </NavButton>
+                    {renderPageButtons()}
+                    <NavButton onClick={handleNext} disabled={pageRange.end === totalPages}>
+                        &gt;&gt;
+                    </NavButton>
+                </PaginationContainer>
+                <div>
+                    <ul>
+                        <li>제목을 클릭하면 상품을 수정할 수 있습니다.</li>
+                        <li>★ 신규 국가/도시 투어 등록은 개발팀에게 문의하세요 ★</li>
+                    </ul>
+                </div>
                 <TableContainer>
                     <StyledTable>
-                    <thead>
-                    <tr>
-                        <th>CODE</th>
-                        <th>UID</th>
-                        <th>Country Id</th>
-                        <th>City Id</th>
-                        <th>Theme</th>
-                        <th>Product Title</th>
-                        <th>Adult</th>
-                        <th>Kids</th>
-                        <th>Start Date</th>
-                        <th>End Date</th>
-                        <th>Min</th>
-                        <th>Max</th>
-                        <th>Status</th>
-                        <th>Type</th>
-                        <th>Review</th>
-                        <th></th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {products.map((p, i) => (
-                        <tr key={p.productCode}>
-                            <td>{p.productCode}</td>
-                            <td>{p.productUid}</td>
-                            <td>{p.countryId}</td>
-                            <td>{p.cityId}</td>
-                            <td>{p.themeCode}</td>
-                            <td>
-                                <Link
-                                    to={`/admin/productEdit/${p.productUid}`}
-                                    style={{color: 'blue'}}
-                                >{p.productTitle}
-                                </Link>
-                            </td>
-                            <td>{p.productAdult.toLocaleString()}</td>
-                            <td>{p.productChild.toLocaleString()}</td>
-                            <td>{p.productStartDate}</td>
-                            <td>{p.productEndDate}</td>
-                            <td>{p.productMinParticipants}</td>
-                            <td>{p.productMaxParticipants}</td>
-                            <td>{p.productStatus}</td>
-                            <td>{p.productType}</td>
-                            <td>{p.reviewCount}</td>
-                            <td>
+                        <thead>
+                        <tr>
+                            <th>CODE</th>
+                            <th>UID</th>
+                            <th>Country Id</th>
+                            <th>City Id</th>
+                            <th>Theme</th>
+                            <th>Product Title</th>
+                            <th>Adult</th>
+                            <th>Kids</th>
+                            <th>Start Date</th>
+                            <th>End Date</th>
+                            <th>Min</th>
+                            <th>Max</th>
+                            <th>Status</th>
+                            <th>Type</th>
+                            <th>Review</th>
+                            <th></th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {products.map((p, i) => (
+                            <tr key={p.productCode}>
+                                <td>{p.productCode}</td>
+                                <td>{p.productUid}</td>
+                                <td>{p.countryId}</td>
+                                <td>{p.cityId}</td>
+                                <td>{p.themeCode}</td>
+                                <td>
+                                    <Link
+                                        to={`/admin/productEdit/${p.productUid}`}
+                                        style={{color: 'blue'}}
+                                    >{p.productTitle}
+                                    </Link>
+                                </td>
+                                <td>{p.productAdult.toLocaleString()}</td>
+                                <td>{p.productChild.toLocaleString()}</td>
+                                <td>{p.productStartDate}</td>
+                                <td>{p.productEndDate}</td>
+                                <td>{p.productMinParticipants}</td>
+                                <td>{p.productMaxParticipants}</td>
+                                <td>{p.productStatus}</td>
+                                <td>{p.productType}</td>
+                                <td>{p.reviewCount}</td>
+                                <td>
                                 <span
                                     onClick={() => onDelete(p.productUid)}
                                     style={{color: 'red', cursor: "pointer"}}
                                 >
                                     삭제
                                 </span>
-                            </td>
-                        </tr>
-                    ))}
-                    </tbody>
+                                </td>
+                            </tr>
+                        ))}
+                        </tbody>
                     </StyledTable>
                 </TableContainer>
             </main>
-
         </div>
     )
 }
