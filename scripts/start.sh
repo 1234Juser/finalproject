c0afb37c9fccf77dbdf27c5c807149a68490ce94
1234Juser <1211shjung@gmail.com>
2025-06-01 14:53:55 +0900

    [docs] 배포준비중

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,26 +1,15 @@
 #!/bin/bash
-APP_DIR="/home/ec2-user/app"
-LOG_PATH="$APP_DIR/deploy.log"
+# /home/ubuntu/app/scripts/start.sh
 
-# 특정 jar 파일을 지정하는 대신, 해당 위치의 모든 jar 파일을 찾습니다.
+APP_DIR="/home/ubuntu/app" # <<< 경로 수정
+LOG_PATH="$APP_DIR/deploy.log"
+ERROR_LOG_PATH="$APP_DIR/error.log"
+
+# 해당 위치의 모든 jar 파일을 찾습니다.
 # build/libs/ 에 jar가 하나만 있다는 가정 하에 동작합니다.
 JAR_PATH=$(ls $APP_DIR/*.jar | head -n 1)
 JAR_NAME=$(basename $JAR_PATH)
 
-echo ">>> 실행할 JAR 파일: $JAR_NAME" >> $LOG_PATH
-
-echo ">>> 현재 실행중인 애플리케이션 pid 확인" >> $LOG_PATH
-# .jar 대신 JAR 파일의 이름으로 프로세스를 찾아서 더 정확하게 식별합니다.
-CURRENT_PID=$(pgrep -f $JAR_NAME)
-
-if [ -z "$CURRENT_PID" ]; then
-    echo ">>> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> $LOG_PATH
-else
-    echo ">>> kill -15 $CURRENT_PID"
-    kill -15 $CURRENT_PID
-    sleep 5
-fi
-
 echo ">>> 새 애플리케이션 배포: $JAR_PATH" >> $LOG_PATH
-# prod 프로파일로 실행합니다.
-nohup java -jar -Dspring.profiles.active=prod $JAR_PATH >> $LOG_PATH 2>&1 &
+# prod 프로파일로 실행하고, 표준 출력과 표준 에러를 각각 다른 파일에 기록합니다.
+nohup java -jar -Dspring.profiles.active=prod $JAR_PATH > $LOG_PATH 2> $ERROR_LOG_PATH &
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-APP_DIR="/home/ec2-user/app"
+# /home/ubuntu/app/scripts/stop.sh
+
+APP_DIR="/home/ubuntu/app" # <<< 경로 수정
 LOG_PATH="$APP_DIR/deploy.log"
 
-echo ">>> 현재 실행중인 애플리케이션 pid 확인" >> $LOG_PATH
-# /home/ec2-user/app/ 경로 아래에서 실행되는 .jar 프로세스를 찾습니다.
+echo ">>> 기존 애플리케이션 pid 확인" >> $LOG_PATH
+# /home/ubuntu/app/ 경로 아래에서 실행되는 .jar 프로세스를 찾습니다.
 CURRENT_PID=$(pgrep -f "$APP_DIR/.*.jar")
 
 if [ -z "$CURRENT_PID" ]; then
     echo ">>> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> $LOG_PATH
 else
-    echo ">>> kill -15 $CURRENT_PID"
+    echo ">>> kill -15 $CURRENT_PID" >> $LOG_PATH
     kill -15 $CURRENT_PID
     sleep 5
 fi
